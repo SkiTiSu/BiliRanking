02e0acd865118723e90af63950fd4b60adfcb368
四季天书SkiTiSu <sjtsjjy@gmail.com>
2016-12-05 01:25:22 +0800

    BiliRanking.Core.Tests: 初步添加单元测试

---
+++ BiliRanking.Core.Tests/BiliInterfaceTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiliRanking.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BiliRanking.Core.Tests
+{
+    [TestClass()]
+    public class BiliInterfaceTests
+    {
+        [TestMethod()]
+        public void GetInfoAsyncTest()
+        {
+            BiliInterfaceInfo info = new BiliInterfaceInfo();
+            info = AsyncHelper.RunSync(() => BiliInterface.GetInfoAsync("av2680512"));
+
+            Assert.AreEqual(info.title, "【创刊号】哔哩哔哩月刊鬼畜排行榜#001");
+        }
+
+        [TestMethod()]
+        public void GetFlvInfoTest()
+        {
+            BiliInterfaceInfo info = new BiliInterfaceInfo();
+            info = BiliInterface.GetFlvInfo("av3153761");
+            //TestContext.WriteLine(info.flvurl);
+            //Debug.WriteLine(info.flvurl);
+
+            Assert.IsTrue(info.flvurl.Contains(".flv"));
+        }
+    }
+}
